0b9fb4a947396a9cd66ed795d655ce31a4c52aa6
yesid <yesidgarzon083@gmail.com>
2023-08-10 07:22:47 -0500

    Inicio proyecto

---
+++ inicio.py
@@ -0,0 +1,8 @@
+print ("--- Calcular nota definitiva ---")
+nombre = input ("Nombre Estudiante: ")
+nota1 = float("Ingrese Nota 1: ")
+nota2 = float("Ingrese Nota 2: ")
+nota3 = float("Ingrese Nota 3: ")
+definitiva = (nota1 + nota2 + nota3)/3
+
+
